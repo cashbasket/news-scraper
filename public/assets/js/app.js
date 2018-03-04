84b44297113c0b27979f3355cc32baa390c9e33e
Ryan Machan <rjm21@case.edu>
2018-03-04 09:06:37 -0500

    begin developing frontend

---
+++ public/assets/js/app.js
@@ -0,0 +1,27 @@
+$.urlParam = function(name, url) {
+	if (!url) {
+		url = window.location.href;
+	}
+	var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+	if (!results) { 
+		return undefined;
+	}
+	return results[1] || undefined;
+};
+
+$(document).ready(function() {
+	if ($.urlParam('scraped') && $.urlParam('scraped') !== '0') {
+		var confirmEnding = $.urlParam('scraped') === '1' ? ' new article was added!' : ' new articles were added!';
+		$('#scrapedConfirm').text($.urlParam('scraped') + confirmEnding)
+			.removeClass('d-none');
+	} else if ($.urlParam('scraped') && $.urlParam('scraped') === '0') {
+		$('#scrapedConfirm').removeClass('alert-success d-none')
+			.addClass('alert-danger')
+			.text('Sorry, no new articles are available.')
+	}
+	$('#scrape, #scrapeBody').on('click', function(event) {
+		$.ajax('/fetch').then(function(response) {
+			location.href = '/?scraped=' + response.length;
+		});
+	});
+});
---
+++ public/assets/js/quotes.js
@@ -0,0 +1,313 @@
+var quotes = [{
+	'source': 'Crazy Taxi',
+	'excerpt': 'Hey hey hey it\'s time to make some carrrrazzzyy money are ya ready? Here we go!'
+}, {
+	'source': 'Diablo II',
+	'excerpt': 'Stay a while, and listen!'
+}, {
+	'source': 'Super Mario Bros.',
+	'excerpt': 'Thank you Mario! But our Princess is in another castle!'
+}, {
+	'source': 'Mario Kart 64',
+	'excerpt': 'I’m-a-Wario! I’ma gonna win!'
+}, {
+	'source': 'Assassin’s Creed II',
+	'excerpt': 'It’s a-me, Mario!'
+}, {
+	'source': 'Oregon Trail',
+	'excerpt': 'You have died of dysentery'
+}, {
+	'source': 'Gauntlet II',
+	'excerpt': 'Blue Wizard is about to die.'
+}, {
+	'source': 'X-Men Arcade',
+	'excerpt': 'X-Men, welcome to die!'
+}, {
+	'source': 'Friday the 13th',
+	'excerpt': 'YOU AND YOUR FRIENDS ARE DEAD.'
+}, {
+	'source': 'Ghosts \'N Goblins',
+	'excerpt': 'Congratulation. This story is happy end. Thank you.'
+}, {
+	'source': 'Ghostbusters',
+	'excerpt': 'Conglaturation! You have completed a great game!'
+}, {
+	'source': 'Pro Wrestling',
+	'excerpt': 'A winner is you'
+}, {
+	'source': 'House of the Dead 2',
+	'excerpt': 'Suffer, like G did.'
+}, {
+	'source': 'House of the Dead: Overkill',
+	'excerpt': 'Motherf***er!'
+}, {
+	'source': 'Bulletstorm',
+	'excerpt': 'I will kill your dicks!'
+}, {
+	'source': 'Portal',
+	'excerpt': 'This is your fault. I\'m going to kill you. And all the cake is gone. You don\'t even care, do you?'
+}, {
+	'source': 'Portal 2',
+	'excerpt': 'Oh, hi. So, how are you holding up? BECAUSE I\'M A POTATO!'
+}, {
+	'source': 'Spec Ops: The Line',
+	'excerpt': 'Everything is teetering on the edge of everything.'
+}, {
+	'source': 'Half-Life 2',
+	'excerpt': 'The right man in the wrong place can make all the difference in the world.'
+}, {
+	'source': 'Unreal Tournament',
+	'excerpt': 'First blood!'
+}, {
+	'source': 'Conker’s Bad Fur Day',
+	'excerpt': 'I am the great mighty poo, and I’m going to throw my shit at you.'
+}, {
+	'source': 'Killer Instinct',
+	'excerpt': 'C-c-c-combo breaker!'
+}, {
+	'source': 'Far Cry 2',
+	'excerpt': 'You can\'t break a man the way you do a dog or a horse - the harder you beat a man, the taller he stands.'
+}, {
+	'source': 'Halo 2',
+	'excerpt': 'Killtacular!'
+}, {
+	'source': 'Castlevania: Symphony of the Night',
+	'excerpt': 'What is a man? A miserable little pile of secrets.'
+}, {
+	'source': 'Unreal Tournament',
+	'excerpt': 'M-M-M-M-MONSTER KILL!'
+}, {
+	'source': 'NBA Jam',
+	'excerpt': 'Boomshakalaka!'
+}, {
+	'source': 'The Secret of Monkey Island',
+	'excerpt': 'Why, that\'s the second biggest monkey head I\'ve ever seen!'
+}, {
+	'source': 'Mortal Kombat',
+	'excerpt': 'Finish him!!'
+}, {
+	'source': 'Mortal Kombat II',
+	'excerpt': 'Toasty!'
+}, {
+	'source': 'Mortal Kombat',
+	'excerpt': 'Get over here!'
+}, {
+	'source': 'Call of Duty 4: Modern Warfare',
+	'excerpt': 'Stay frosty.'
+}, {
+	'source': 'Impossible Mission',
+	'excerpt': 'Oh look, another visitor. Stay awhile... Stay FOREVER!'
+}, {
+	'source': 'Altered Beast',
+	'excerpt': 'Welcome to your doom!'
+}, {
+	'source': 'Altered Beast',
+	'excerpt': 'Rise from your grave!'
+}, {
+	'source': 'BioShock',
+	'excerpt': 'Is a man not entitled to the sweat of his brow?'
+}, {
+	'source': 'BioShock',
+	'excerpt': 'A man chooses; a slave obeys.'
+}, {
+	'source': 'BioShock',
+	'excerpt': 'Would you kindly...'
+}, {
+	'source': 'Metal Gear Solid 2',
+	'excerpt': 'La Li Lu Le Lo'
+}, {
+	'source': 'Metal Gear',
+	'excerpt': 'I feel asleep!!'
+}, {
+	'source': 'Metal Gear Solid',
+	'excerpt': 'Snake? Snake? SNAAAAAAAAKE!!!'
+}, {
+	'source': 'Metal Gear Solid 4',
+	'excerpt': 'War has changed.'
+}, {
+	'source': 'Fallout 3',
+	'excerpt': 'War. War never changes.'
+}, {
+	'source': 'StarCraft',
+	'excerpt': 'Nuclear launch detected.'
+}, {
+	'source': 'StarCraft',
+	'excerpt': 'You must construct additional pylons.'
+}, {
+	'source': 'Mega Man II',
+	'excerpt': 'In the year 200x a super robot named Mega Man was created.'
+}, {
+	'source': 'Chrono Trigger',
+	'excerpt': 'They call me Gato, I have metal joints. Beat me up and win 15 Silver Points.'
+}, {
+	'source': 'Halo',
+	'excerpt': 'I need a weapon.'
+}, {
+	'source': 'Halo 3',
+	'excerpt': 'Wake me when you need me.'
+}, {
+	'source': 'Wii Sports',
+	'excerpt': 'Why not take a break? You can pause the game by pressing +.'
+}, {
+	'source': 'Final Fantasy II',
+	'excerpt': 'You spoony bard!'
+}, {
+	'source': 'Street Fighter II',
+	'excerpt': 'Sonic Boom!'
+}, {
+	'source': 'Every Sega Genesis game',
+	'excerpt': 'Segaaaaaaaaaaaaaaaaaaaaa.'
+}, {
+	'source': 'Street Fighter II',
+	'excerpt': 'Hadouken!'
+}, {
+	'source': 'EA Sports games',
+	'excerpt': 'EA Sports! It’s in the game.'
+}, {
+	'source': 'Street Fighter II',
+	'excerpt': 'Shoryuken!'
+}, {
+	'source': 'Final Fight',
+	'excerpt': 'Oh! My car!'
+}, {
+	'source': 'Team Fortress 2',
+	'excerpt': 'Spy\'s sappin’ my sentry!'
+}, {
+	'source': 'Sim City 2000',
+	'excerpt': 'Reticulating splines.'
+}, {
+	'source': 'Bad Dudes',
+	'excerpt': 'The President has been kidnapped by ninjas. Are you a bad enough dude to rescue the president?'
+}, {
+	'source': 'Grand Theft Auto IV',
+	'excerpt': 'Big American titties!'
+}, {
+	'source': 'Bad Dudes',
+	'excerpt': 'Hey dudes thanks, for rescuing me. Let\'s go for a burger....Ha! Ha! Ha! Ha!'
+}, {
+	'source': 'Assassin’s Creed II',
+	'excerpt': 'Requiescat in pace.'
+}, {
+	'source': 'Prince of Persia: The Sands of Time',
+	'excerpt': 'Wait, that’s not how it happened.'
+}, {
+	'source': 'Phoenix Wright: Ace Attorney',
+	'excerpt': 'Objection!'
+}, {
+	'source': 'Resident Evil 4',
+	'excerpt': 'Leon!'
+}, {
+	'source': 'Pac-Man',
+	'excerpt': 'Wakka wakka wakka!'
+}, {
+	'source': 'Journey',
+	'excerpt': 'Wyonk wyonk wyonk… wyonk wyonk.'
+}, {
+	'source': 'God of War',
+	'excerpt': 'AREEEEEEEEEES!!!!!!!!!'
+}, {
+	'source': 'Timesplitters: Future Perfect',
+	'excerpt': 'It’s TIME TO SPLIT!'
+}, {
+	'source': 'Dead Rising',
+	'excerpt': 'I’ve covered wars, you know.'
+}, {
+	'source': 'Resident Evil',
+	'excerpt': 'You were almost a Jill sandwich!'
+}, {
+	'source': 'Resident Evil',
+	'excerpt': 'Jill, here\'s a lockpick. It might come in handy if you, the master of unlocking, take it with you.'
+}, {
+	'source': 'Warcraft II',
+	'excerpt': 'Job\'s done.'
+}, {
+	'source': 'Command & Conquer: Generals',
+	'excerpt': 'I need shoes!'
+}, {
+	'source': 'Super Smash Bros.',
+	'excerpt': 'Show me your moves!'
+}, {
+	'source': 'Gears of War',
+	'excerpt': 'REVIVE ME!'
+}, {
+	'source': 'Sam & Max Hit the Road',
+	'excerpt': 'Good thinking, little buddy.'
+}, {
+	'source': 'Half-Life',
+	'excerpt': '…'
+}, {
+	'source': 'Professor Layton and the Curious Village',
+	'excerpt': 'Every puzzle has an answer.'
+}, {
+	'source': 'The Legend of Zelda',
+	'excerpt': 'It’s dangerous to go alone; take this!'
+}, {
+	'source': 'The Legend of Zelda II',
+	'excerpt': 'I am error.'
+}, {
+	'source': 'The Legend of Zelda: Ocarina of Time',
+	'excerpt': 'Hey! Listen!'
+}, {
+	'source': 'Professor Layton and the Curious Village',
+	'excerpt': 'Frankly, I’m ashamed.'
+}, {
+	'source': 'LA Noire',
+	'excerpt': 'A nosy old hag like you knows everything about the people who live under her roof. Where did she go?'
+}, {
+	'source': 'Zork',
+	'excerpt': 'It is pitch black. You are likely to be eaten by a grue.'
+}, {
+	'source': 'Smash TV',
+	'excerpt': 'Big...money! Big...prizes! I love it!'
+}, {
+	'source': 'Viewtiful Joe',
+	'excerpt': 'Henshin a go-go, baby!'
+}, {
+	'source': 'Gears of War',
+	'excerpt': 'The Train’s at home on the rails!'
+}, {
+	'source': 'The Elder Scrolls V: Skyrim',
+	'excerpt': 'Fus-ro-dah!'
+}, {
+	'source': 'Luigi’s Mansion',
+	'excerpt': 'Maaaaaaaaario?'
+}, {
+	'source': 'Earthbound',
+	'excerpt': 'Say, fuzzy pickles.'
+}, {
+	'source': 'League of Legends',
+	'excerpt': 'Welcome to Summoner’s Rift.'
+}, {
+	'source': 'Pokemon series',
+	'excerpt': 'It’s super effective!'
+}, {
+	'source': 'Heavy Rain',
+	'excerpt': 'Jason! Jaaaaaaaaason!'
+}, {
+	'source': 'Dragon’s Lair',
+	'excerpt': 'Save me!'
+}, {
+	'source': 'Bastion',
+	'excerpt': 'The Kid just rages for a while.'
+}, {
+	'source': 'Zero Wing',
+	'excerpt': 'All your base are belong to us.'
+}, {
+	'source': 'Star Fox',
+	'excerpt': 'Do a barrel roll!'
+}];
+
+
+//global math functions
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$(document).ready(function() {
+	$('.excerpt').each(function(){
+		if(!$(this).text().length) {
+			var randomQuote = quotes[getRandomInt(0, quotes.length - 1)];
+			$(this).html('<div class="no-excerpt alert alert-primary"><p><em><strong>WELP</strong>. This article has no excerpt, so here\'s a random video game quote:</em></p><blockquote class="blockquote text-center"><p class="mb-0">&ldquo;' + randomQuote.excerpt + '&rdquo;</p><footer class="blockquote-footer">From  <cite>' + randomQuote.source + '</cite></footer></blockquote></div>');
+		}
+	});
+});
