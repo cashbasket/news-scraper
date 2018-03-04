a3762abdf9785edd48357b20fb69fd4fe564f4e2
Ryan Machan <rjm21@case.edu>
2018-03-04 09:05:30 -0500

    create models for mongoose

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	link: {
+		type: String,
+		required: true
+	},
+	excerpt: {
+		type: String,
+		required: false
+	},
+	image: {
+		type: String,
+		required: false
+	},
+	postedAt: {
+		type: Date,
+		required: true
+	},
+	scrapedAt: {
+		type: Date,
+		default: Date.now,
+		required: true
+	},
+	note: {
+		type: Schema.Types.ObjectId,
+		ref: 'Note'
+	}
+});
+
+var Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
